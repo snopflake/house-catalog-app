c77b0528bc6aa68e3452a16d7a457bb50864b207
lidwinae <lidwinaeleonora@gmail.com>
2025-06-06 22:44:37 +0700

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-// filepath: /home/praktikum/house-catalog-app/frontend/src/App.js
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import UploadPage from './pages/UploadPage';
 import GalleryPage from './pages/GalleryPage';
+import LandingPage from './pages/LandingPage';
 import './App.css';
 
 function App() {
@@ -11,23 +11,26 @@ function App() {
 
   return (
     <Router>
-      {!token ? (
-        <Routes>
-          <Route path="/login" element={<LoginPage setToken={setToken} />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      ) : (
-        <div>
-          <button onClick={() => {
-            localStorage.removeItem('token');
-            setToken('');
-          }}>Logout</button>
-          <Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {!token ? (
+          <>
+            <Route path="/login" element={<LoginPage setToken={setToken} />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        ) : (
+          <>
             <Route path="/upload" element={<UploadPage />} />
             <Route path="/gallery" element={<GalleryPage />} />
             <Route path="*" element={<Navigate to="/upload" />} />
-          </Routes>
-        </div>
+          </>
+        )}
+      </Routes>
+      {token && (
+        <button onClick={() => {
+          localStorage.removeItem('token');
+          setToken('');
+        }}>Logout</button>
       )}
     </Router>
   );
