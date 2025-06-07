ad6d16763031747e064c513c67c9bebbc0eceafc
lidwinae <lidwinaeleonora@gmail.com>
2025-06-07 18:05:46 +0700

    change footer name

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-[#2C3E50] text-center py-4 mt-8">
-      <p className="text-white text-sm">&copy; 2025 House Catalog App</p>
+      <p className="text-white text-sm">&copy; 2025 Dream House Design App</p>
     </footer>
   );
 }
